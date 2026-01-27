4f43d33da79d1bb372ee266736540ee5079606ac
ERICK MEDEIROS BRITO DA SILVA <a95607457@rn.senac.local>
2026-01-27 20:02:46 -0300

    feito

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import HomePage from "../components/homePage/HomePage";
 
 export default function Home() {
   return <HomePage />;
 }
-=======
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  redirect('/login');
-}
-
->>>>>>> origin/develop
