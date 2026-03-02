d1a8349cb1ce3a1654aed5c26126fc1ab8281a73
ERICK MEDEIROS BRITO DA SILVA <a95607457@rn.senac.local>
2026-03-02 19:37:07 -0300

    Fix: remover pasta config com arquivo json incorreto

--- config.cjs
+++ config.cjs
@@ -1,23 +1,23 @@
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'sa',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'database_development',
-    host: process.env.DB_HOST || '127.0.0.1',
-    dialect: process.env.DB_DIALECT || 'mssql'
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME ,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   },
   test: {
-    username: process.env.DB_USER || 'sa',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'database_test',
-    host: process.env.DB_HOST || '127.0.0.1',
-    dialect: process.env.DB_DIALECT || 'mssql'
+    username: process.env.DB_USER  ,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME ,
+    host: process.env.DB_HOST ,
+    dialect: process.env.DB_DIALECT
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mssql'
+    dialect: process.env.DB_DIALECT
   }
 };
